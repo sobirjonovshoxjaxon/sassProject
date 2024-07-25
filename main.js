5a2fc1eaf8e17453c76c7cffa7348ec39882fb79
shoxjaxon <shohjahonsobirjonov76@gmail.com>
2024-07-25 23:15:39 +0500

    I wrote contact part

---
+++ main.js
@@ -0,0 +1,24 @@
+//homepage carausel
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+  });
+//homepage carausel
+
+//testimonial carausel
+var swiper = new Swiper(".mySwiper2", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+});
+//testimonial carausel
+
